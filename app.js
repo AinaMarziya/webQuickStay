4286f9b7af073fd90e13c7ce923e6a04b7b1e09a
AinaMarziya <ainamarziya727@gmail.com>
2025-12-17 09:49:55 -0500

    Fix session config and prepare app for Render deployment

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const path=require("path");
 const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
 const ExpressError =require("./utils/ExpressError.js");
-const session= require("express-session");
-const MongoStore = require("connect-mongo"); //ABHI KIYA
+const session = require("express-session");
+const MongoStore = require("connect-mongo");//ABHI KIYA
 const flash=require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -43,57 +43,48 @@ app.engine('ejs', ejsMate);
 app.use(express.static("public"));
 
 
-// const store = MongoStore.create({
-//     mongoUrl: dbUrl,
-//     crypto: { 
-//         secret: process.env.SECRET,
-//     },
-//     touchAfter: 24 * 3600,
-// });
-
 const store = MongoStore.create({
-  mongoUrl: dbUrl,
-  collectionName: "sessions",
-  crypto: {
-    secret: process.env.SECRET,
-  },
+    mongoUrl: dbUrl,
+    // crypto: { 
+    //     secret: process.env.SECRET,
+    // },
+    touchAfter: 24 * 3600,
 });
 
 
-store.on("error", (err) => {
-    console.log("ERROR in MONGO SESSSION STORE", err);
-});
-
-const sessionOptions = {
-    store,
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-    expires: Date.now() + 7 * 24 *60 * 60 * 1000,
-    maxAge: 7 * 24 *60 * 60 * 1000,
+app.use(session({
+  secret: process.env.SESSION_SECRET || "thisisasecret",
+  resave: false,
+  saveUninitialized: false,
+  store,
+  cookie: {
     httpOnly: true,
-    },
-};
-  
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  }
+}));
 
 
-app.use(session(sessionOptions));
+
+
 app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use((req, res, next ) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.currUser = req.user || null;
+    next();
+});
+
 passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req, res, next ) => {
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
-    next();
-});
+
 
 //Fix favicon error
 app.get('/favicon.ico', (req, res) => res.status(204).end());
